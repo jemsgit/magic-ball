5949d7efc2ebae115773f7bce71aecbbcdde0683
jem jem <jemguns@gmail.com>
2024-12-30 14:50:22 +0400

    ga

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,29 @@ export default defineConfig(({ mode }) => {
   let base = isProduction ? "/ball/" : "/";
 
   return {
+    plugins: [
+      {
+        name: "html-transform",
+        transformIndexHtml(html) {
+          if (isProduction) {
+            // Inject Google Analytics code only in production
+            return html.replace(
+              "</head>",
+              `<script async src="https://www.googletagmanager.com/gtag/js?id=G-3J8TLC4ES0"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'G-3J8TLC4ES0');
+              </script>
+              </head>`
+            );
+          }
+          return html;
+        },
+      },
+    ],
     base: base,
   };
 });
